bbc7f52ac8119b8f39fea476f696344e7118cc4c
Liceth Yaneth Rozo Quintana <l.rozoq@uniandes.edu.co>
2024-09-14 20:35:49 -0500

    Creacion de modulos de productos, tiendas y productos-tiendas

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,12 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { ProductosModule } from './productos/productos.module';
+import { TiendasModule } from './tiendas/tiendas.module';
+import { ProductosTiendasModule } from './productos-tiendas/productos-tiendas.module';
 
 @Module({
-  imports: [],
+  imports: [ProductosModule, TiendasModule, ProductosTiendasModule],
   controllers: [AppController],
   providers: [AppService],
 })
