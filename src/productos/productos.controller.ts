02c106d9f92b8583cd30ee0582103151049b99ec
Liceth Yaneth Rozo Quintana <l.rozoq@uniandes.edu.co>
2024-09-15 23:05:48 -0500

    Creacion de controladores de productos

---
+++ src/productos/productos.controller.ts
@@ -0,0 +1,43 @@
+import { Body, Controller, Delete, Get, HttpCode, Param, Post, Put, UseInterceptors } from '@nestjs/common';
+import { ProductosService } from './productos.service';
+import { ProductoEntity } from './entities/producto-entity';
+import { plainToInstance } from 'class-transformer';
+import { ProductoDto } from './dto/producto.dto';
+import { BusinessErrorsInterceptor } from 'src/shared/interceptors/business-errors/business-errors.interceptor';
+
+@Controller('products')
+@UseInterceptors(BusinessErrorsInterceptor)
+export class ProductosController {
+
+  constructor(
+    private readonly productoService: ProductosService
+  ) {}
+
+  @Get()
+  async findAll() {
+    return await this.productoService.findAll();
+  }
+
+  @Get(":productoId")
+  async findOne(@Param("productoId") productoId: string) {
+    return await this.productoService.findOne(productoId);
+  }
+
+  @Post()
+  async create(@Body() productoDto: ProductoDto) {
+    const producto:ProductoEntity = plainToInstance(ProductoEntity, productoDto);
+    return await this.productoService.create(producto);
+  }
+
+  @Put(':productoId')
+  async update(@Param('productoId') productoId: string, @Body() productoDto: ProductoDto) {
+    const producto: ProductoEntity = plainToInstance(ProductoEntity, productoDto);
+    return await this.productoService.update(productoId, producto);
+  }
+
+  @Delete(':productoId')
+  @HttpCode(204)
+  async delete(@Param('productoId') productoId: string) {
+    return await this.productoService.delete(productoId);
+  }
+}
--- src/productos/productos.module.ts
+++ src/productos/productos.module.ts
@@ -2,9 +2,11 @@ import { Module } from '@nestjs/common';
 import { ProductosService } from './productos.service';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { ProductoEntity } from './entities/producto-entity';
+import { ProductosController } from './productos.controller';
 
 @Module({
   imports: [TypeOrmModule.forFeature([ProductoEntity])],
   providers: [ProductosService],
+  controllers: [ProductosController],
 })
 export class ProductosModule {}
